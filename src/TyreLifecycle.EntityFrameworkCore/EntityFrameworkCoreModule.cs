feaf5bbb2cec4f8caac07c600e9f78c15aca0fa6
Zikode03 <ngcebonkwanyana03@gmail.com>
2026-08-18 20:54:55 +0200

    Register specific ABP repositories

--- src/TyreLifecycle.EntityFrameworkCore/EntityFrameworkCoreModule.cs
+++ src/TyreLifecycle.EntityFrameworkCore/EntityFrameworkCoreModule.cs
@@ -1,20 +1,27 @@
 using Microsoft.Extensions.DependencyInjection;
+using TyreLifecycle.Customers;
+using TyreLifecycle.EntityFrameworkCore.Customers;
+using TyreLifecycle.EntityFrameworkCore.Tyres;
+using TyreLifecycle.EntityFrameworkCore.Vehicles;
+using TyreLifecycle.Tyres;
+using TyreLifecycle.Vehicles;
 using Volo.Abp.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore.SqlServer;
 using Volo.Abp.Modularity;
 
 namespace TyreLifecycle.EntityFrameworkCore;
 
-[DependsOn(
-    typeof(AbpEntityFrameworkCoreSqlServerModule)
-)]
+[DependsOn(typeof(AbpEntityFrameworkCoreSqlServerModule))]
 public class TyreLifecycleEntityFrameworkCoreModule : AbpModule
 {
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
         context.Services.AddAbpDbContext<TyreLifecycleDbContext>(options =>
         {
-            options.AddDefaultRepositories(includeAllEntities: true);
+            options.AddDefaultRepositories(includeAllEntities: false);
+            options.AddRepository<Customer, EfCoreCustomerRepository>();
+            options.AddRepository<Vehicle, EfCoreVehicleRepository>();
+            options.AddRepository<Tyre, EfCoreTyreRepository>();
         });
 
         Configure<AbpDbContextOptions>(options =>
