8a763747c16b64e5ca697b020ea22c57b2a82f34
Zikode03 <ngcebonkwanyana03@gmail.com>
2026-08-18 21:15:04 +0200

    Enforce vehicle permissions and use ObjectMapper

--- src/TyreLifecycle.Application/Vehicles/VehicleAppService.cs
+++ src/TyreLifecycle.Application/Vehicles/VehicleAppService.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
+using TyreLifecycle.Permissions;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 
 namespace TyreLifecycle.Vehicles;
 
+[Authorize(TyreLifecyclePermissions.Vehicles.Default)]
 public class VehicleAppService : ApplicationService, IVehicleAppService
 {
     private readonly IVehicleRepository _vehicleRepository;
@@ -14,47 +17,51 @@ public class VehicleAppService : ApplicationService, IVehicleAppService
         _vehicleManager = vehicleManager;
     }
 
-    public async Task<VehicleDto> GetAsync(Guid id) => Map(await _vehicleRepository.GetAsync(id));
+    public async Task<VehicleDto> GetAsync(Guid id)
+        => ObjectMapper.Map<Vehicle, VehicleDto>(await _vehicleRepository.GetAsync(id));
 
     public async Task<PagedResultDto<VehicleDto>> GetListAsync(PagedAndSortedResultRequestDto input)
     {
         var totalCount = await _vehicleRepository.GetCountAsync();
-        var items = await _vehicleRepository.GetPagedListAsync(input.SkipCount, input.MaxResultCount, input.Sorting ?? nameof(Vehicle.CreationTime), true);
-        return new PagedResultDto<VehicleDto>(totalCount, items.Select(Map).ToList());
+        var items = await _vehicleRepository.GetPagedListAsync(
+            input.SkipCount,
+            input.MaxResultCount,
+            input.Sorting ?? nameof(Vehicle.CreationTime),
+            includeDetails: true);
+
+        return new PagedResultDto<VehicleDto>(
+            totalCount,
+            ObjectMapper.Map<List<Vehicle>, List<VehicleDto>>(items));
     }
 
+    [Authorize(TyreLifecyclePermissions.Vehicles.Create)]
     public async Task<VehicleDto> CreateAsync(CreateVehicleDto input)
     {
-        var vehicle = await _vehicleManager.CreateAsync(input.CustomerId, input.RegistrationNumber, input.Make, input.Model, input.Year, input.OdometerKm, input.Vin);
+        var vehicle = await _vehicleManager.CreateAsync(
+            input.CustomerId,
+            input.RegistrationNumber,
+            input.Make,
+            input.Model,
+            input.Year,
+            input.OdometerKm,
+            input.Vin);
+
         await _vehicleRepository.InsertAsync(vehicle, autoSave: true);
-        return Map(vehicle);
+        return ObjectMapper.Map<Vehicle, VehicleDto>(vehicle);
     }
 
+    [Authorize(TyreLifecyclePermissions.Vehicles.Update)]
     public async Task<VehicleDto> UpdateAsync(Guid id, UpdateVehicleDto input)
     {
         var vehicle = await _vehicleRepository.GetAsync(id);
         vehicle.UpdateOdometer(input.OdometerKm);
         vehicle.SetActive(input.IsActive);
+
         await _vehicleRepository.UpdateAsync(vehicle, autoSave: true);
-        return Map(vehicle);
+        return ObjectMapper.Map<Vehicle, VehicleDto>(vehicle);
     }
 
-    public Task DeleteAsync(Guid id) => _vehicleRepository.DeleteAsync(id, autoSave: true);
-
-    private static VehicleDto Map(Vehicle vehicle) => new()
-    {
-        Id = vehicle.Id,
-        CustomerId = vehicle.CustomerId,
-        RegistrationNumber = vehicle.RegistrationNumber,
-        Make = vehicle.Make,
-        Model = vehicle.Model,
-        Year = vehicle.Year,
-        OdometerKm = vehicle.OdometerKm,
-        Vin = vehicle.Vin,
-        IsActive = vehicle.IsActive,
-        CreationTime = vehicle.CreationTime,
-        CreatorId = vehicle.CreatorId,
-        LastModificationTime = vehicle.LastModificationTime,
-        LastModifierId = vehicle.LastModifierId
-    };
+    [Authorize(TyreLifecyclePermissions.Vehicles.Delete)]
+    public Task DeleteAsync(Guid id)
+        => _vehicleRepository.DeleteAsync(id, autoSave: true);
 }
