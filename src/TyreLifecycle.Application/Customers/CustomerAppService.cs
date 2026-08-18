206bae67f8b76dec898ad5b0018fb91ffda72915
Zikode03 <ngcebonkwanyana03@gmail.com>
2026-08-18 21:14:46 +0200

    Enforce customer permissions and use ObjectMapper

--- src/TyreLifecycle.Application/Customers/CustomerAppService.cs
+++ src/TyreLifecycle.Application/Customers/CustomerAppService.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
+using TyreLifecycle.Permissions;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 
 namespace TyreLifecycle.Customers;
 
+[Authorize(TyreLifecyclePermissions.Customers.Default)]
 public class CustomerAppService : ApplicationService, ICustomerAppService
 {
     private readonly ICustomerRepository _customerRepository;
@@ -14,45 +17,49 @@ public class CustomerAppService : ApplicationService, ICustomerAppService
         _customerManager = customerManager;
     }
 
-    public async Task<CustomerDto> GetAsync(Guid id) => Map(await _customerRepository.GetAsync(id));
+    public async Task<CustomerDto> GetAsync(Guid id)
+        => ObjectMapper.Map<Customer, CustomerDto>(await _customerRepository.GetAsync(id));
 
     public async Task<PagedResultDto<CustomerDto>> GetListAsync(PagedAndSortedResultRequestDto input)
     {
         var totalCount = await _customerRepository.GetCountAsync();
-        var items = await _customerRepository.GetPagedListAsync(input.SkipCount, input.MaxResultCount, input.Sorting ?? nameof(Customer.CreationTime), true);
-        return new PagedResultDto<CustomerDto>(totalCount, items.Select(Map).ToList());
+        var items = await _customerRepository.GetPagedListAsync(
+            input.SkipCount,
+            input.MaxResultCount,
+            input.Sorting ?? nameof(Customer.CreationTime),
+            includeDetails: true);
+
+        return new PagedResultDto<CustomerDto>(
+            totalCount,
+            ObjectMapper.Map<List<Customer>, List<CustomerDto>>(items));
     }
 
+    [Authorize(TyreLifecyclePermissions.Customers.Create)]
     public async Task<CustomerDto> CreateAsync(CreateCustomerDto input)
     {
-        var customer = await _customerManager.CreateAsync(input.CustomerNumber, input.FirstName, input.LastName, input.MobileNumber, input.Email);
+        var customer = await _customerManager.CreateAsync(
+            input.CustomerNumber,
+            input.FirstName,
+            input.LastName,
+            input.MobileNumber,
+            input.Email);
+
         await _customerRepository.InsertAsync(customer, autoSave: true);
-        return Map(customer);
+        return ObjectMapper.Map<Customer, CustomerDto>(customer);
     }
 
+    [Authorize(TyreLifecyclePermissions.Customers.Update)]
     public async Task<CustomerDto> UpdateAsync(Guid id, UpdateCustomerDto input)
     {
         var customer = await _customerRepository.GetAsync(id);
         customer.UpdateContact(input.MobileNumber, input.Email);
         customer.SetActive(input.IsActive);
+
         await _customerRepository.UpdateAsync(customer, autoSave: true);
-        return Map(customer);
+        return ObjectMapper.Map<Customer, CustomerDto>(customer);
     }
 
-    public Task DeleteAsync(Guid id) => _customerRepository.DeleteAsync(id, autoSave: true);
-
-    private static CustomerDto Map(Customer customer) => new()
-    {
-        Id = customer.Id,
-        CustomerNumber = customer.CustomerNumber,
-        FirstName = customer.FirstName,
-        LastName = customer.LastName,
-        MobileNumber = customer.MobileNumber,
-        Email = customer.Email,
-        IsActive = customer.IsActive,
-        CreationTime = customer.CreationTime,
-        CreatorId = customer.CreatorId,
-        LastModificationTime = customer.LastModificationTime,
-        LastModifierId = customer.LastModifierId
-    };
+    [Authorize(TyreLifecyclePermissions.Customers.Delete)]
+    public Task DeleteAsync(Guid id)
+        => _customerRepository.DeleteAsync(id, autoSave: true);
 }
