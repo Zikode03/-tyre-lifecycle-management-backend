e394c14fc336acd4953d63f35b4a18447944a3f8
Zikode03 <ngcebonkwanyana03@gmail.com>
2026-08-18 13:05:44 +0200

    Add Customer aggregate

---
+++ src/TyreLifecycle.Domain/Customers/Customer.cs
@@ -0,0 +1,34 @@
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace TyreLifecycle.Customers;
+
+public class Customer : FullAuditedAggregateRoot<Guid>
+{
+    public string CustomerNumber { get; private set; } = string.Empty;
+    public string FirstName { get; private set; } = string.Empty;
+    public string LastName { get; private set; } = string.Empty;
+    public string MobileNumber { get; private set; } = string.Empty;
+    public string? Email { get; private set; }
+    public bool IsActive { get; private set; }
+
+    protected Customer() { }
+
+    public Customer(Guid id, string customerNumber, string firstName, string lastName, string mobileNumber, string? email = null)
+        : base(id)
+    {
+        CustomerNumber = customerNumber;
+        FirstName = firstName;
+        LastName = lastName;
+        MobileNumber = mobileNumber;
+        Email = email;
+        IsActive = true;
+    }
+
+    public void UpdateContact(string mobileNumber, string? email)
+    {
+        MobileNumber = mobileNumber;
+        Email = email;
+    }
+
+    public void SetActive(bool isActive) => IsActive = isActive;
+}
