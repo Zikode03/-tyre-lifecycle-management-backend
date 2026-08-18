25f51bfb3af85830d38e708a6d13a89c1e0f8cd2
Zikode03 <ngcebonkwanyana03@gmail.com>
2026-08-18 21:14:30 +0200

    Configure application AutoMapper

--- src/TyreLifecycle.Application/TyreLifecycleApplicationModule.cs
+++ src/TyreLifecycle.Application/TyreLifecycleApplicationModule.cs
@@ -1,13 +1,22 @@
 using Volo.Abp.Application;
+using Volo.Abp.AutoMapper;
 using Volo.Abp.Modularity;
 
 namespace TyreLifecycle;
 
 [DependsOn(
     typeof(AbpDddApplicationModule),
+    typeof(AbpAutoMapperModule),
     typeof(TyreLifecycleDomainModule),
     typeof(TyreLifecycleApplicationContractsModule)
 )]
 public class TyreLifecycleApplicationModule : AbpModule
 {
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpAutoMapperOptions>(options =>
+        {
+            options.AddMaps<TyreLifecycleApplicationModule>(validate: true);
+        });
+    }
 }
