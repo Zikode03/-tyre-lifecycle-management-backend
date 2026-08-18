63439957e21a551b85ea5c25604f3df2b6de868d
Zikode03 <ngcebonkwanyana03@gmail.com>
2026-08-18 21:22:17 +0200

    Map inspection aggregate in EF Core

--- src/TyreLifecycle.EntityFrameworkCore/TyreLifecycleDbContext.cs
+++ src/TyreLifecycle.EntityFrameworkCore/TyreLifecycleDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using TyreLifecycle.Customers;
+using TyreLifecycle.Inspections;
 using TyreLifecycle.Tyres;
 using TyreLifecycle.Vehicles;
 using Volo.Abp.Data;
@@ -13,6 +14,8 @@ public class TyreLifecycleDbContext : AbpDbContext<TyreLifecycleDbContext>
     public DbSet<Customer> Customers => Set<Customer>();
     public DbSet<Vehicle> Vehicles => Set<Vehicle>();
     public DbSet<Tyre> Tyres => Set<Tyre>();
+    public DbSet<Inspection> Inspections => Set<Inspection>();
+    public DbSet<InspectionTyreReading> InspectionTyreReadings => Set<InspectionTyreReading>();
 
     public TyreLifecycleDbContext(DbContextOptions<TyreLifecycleDbContext> options)
         : base(options)
@@ -59,6 +62,42 @@ public class TyreLifecycleDbContext : AbpDbContext<TyreLifecycleDbContext>
             b.Property(x => x.TreadDepthMm).HasPrecision(4, 1);
             b.HasIndex(x => x.TyreNumber).IsUnique();
             b.HasIndex(x => x.VehicleId);
+        });
+
+        builder.Entity<Inspection>(b =>
+        {
+            b.ToTable(TyreLifecycleConsts.DbTablePrefix + "Inspections", TyreLifecycleConsts.DbSchema);
+            b.ConfigureByConvention();
+            b.Property(x => x.InspectionNumber).IsRequired().HasMaxLength(32);
+            b.Property(x => x.Notes).HasMaxLength(2000);
+            b.HasIndex(x => x.InspectionNumber).IsUnique();
+            b.HasIndex(x => x.VehicleId);
+            b.HasIndex(x => x.Status);
+
+            b.HasMany(x => x.Readings)
+                .WithOne()
+                .HasForeignKey(x => x.InspectionId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            b.Navigation(x => x.Readings)
+                .UsePropertyAccessMode(PropertyAccessMode.Field);
+        });
+
+        builder.Entity<InspectionTyreReading>(b =>
+        {
+            b.ToTable(TyreLifecycleConsts.DbTablePrefix + "InspectionTyreReadings", TyreLifecycleConsts.DbSchema);
+            b.ConfigureByConvention();
+            b.Property(x => x.Position).IsRequired().HasMaxLength(32);
+            b.Property(x => x.InnerTreadMm).HasPrecision(4, 1);
+            b.Property(x => x.CentreTreadMm).HasPrecision(4, 1);
+            b.Property(x => x.OuterTreadMm).HasPrecision(4, 1);
+            b.Property(x => x.PressurePsi).HasPrecision(5, 1);
+            b.Property(x => x.WearPattern).HasMaxLength(128);
+            b.Property(x => x.Defects).HasMaxLength(1000);
+            b.Property(x => x.Recommendation).HasMaxLength(1000);
+            b.HasIndex(x => x.InspectionId);
+            b.HasIndex(x => x.TyreId);
+            b.HasIndex(x => new { x.InspectionId, x.Position }).IsUnique();
         });
     }
 }
